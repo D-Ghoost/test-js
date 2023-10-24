77ef50762dd2807e4ab011ad7e3a258176227c28
Nicolas Torres López <70347270+D-Ghoost@users.noreply.github.com>
2023-10-23 21:17:43 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 
 
 function isEven(number){
-    return true;
+    if (number%2==0){
+        return true;
+    } else {
+        return false;
+    }       
 }
 
 function fibonacci(number){
-    return [];
+    i = 1;
+    j = i;
+    var arrai = [];
+    while (i < number){
+        arrai.push(i);
+        i+=j;
+        arrai.push(j);
+        j+=i;
+    }
+    return arrai;
+    
 }
+function orderNumber(){
+order.sort(function(a, b) {
+        return a - b;
+      });}
 
-function orderNumber(arr){
-    return arr;
-}
 
 
 module.exports ={
